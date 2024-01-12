c0bd5fcfe8170af7a255e42b2596f2a7c2f8f4e0
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2024-01-12 17:39:31 +0200

    correct-card

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,11 +6,11 @@ export class Product extends Component {
       return (
           <div className={styles.product_item}>
               <img src={"./img/" + this.props.item.img} alt="Product" width="300px" height="300px" className={styles.product_img} />
-              <h2 className="product-title">{this.props.item.title}</h2>
-              <p className="product-desc">{this.props.item.desc}</p>
-              <p className="product-category">{this.props.item.category}</p>
-              <span className="product-price">{this.props.item.price} $</span>
-              <div className="product-add-card">+</div>
+              <h2 className={styles.product_title}>{this.props.item.title}</h2>
+              <p className={styles.product_desc}>{this.props.item.desc}</p>
+              <p className={styles.product_category}>{this.props.item.category}</p>
+              <span className={styles.product_price}>{this.props.item.price} $</span>
+              <div className={styles.product_add_card}>+</div>
           </div>
       );
   }
