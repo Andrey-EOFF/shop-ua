8bc35d4ac22ee76573b6a77e26fabe7e2d3f557f
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2024-01-15 12:38:33 +0200

    shop-cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { FaShoppingBasket } from "react-icons/fa";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
+
   return (
     <header>
-          <div className='header'>
-              <span className='logo'>Home UA</span>
-              <div className='nav'>
-              <ul className='nav-list'>
-                  <li className='nav-item'>Про нас</li>
-                  <li className='nav-item'>Контакти</li>
-                  <li className='nav-item'>Кабінет</li>
-              </ul>
-              </div>
-          </div>
-          <div className='presentation'></div>
+      <div className="header">
+        <span className="logo">Home UA</span>
+        <div className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">Про нас</li>
+            <li className="nav-item">Контакти</li>
+            <li className="nav-item">Кабінет</li>
+          </ul>
+          <FaShoppingBasket onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop_card_button ${cartOpen && 'active'}`} />
+          {cartOpen && (
+            <div className="shop_cart"></div>
+          )}
+        </div>
+      </div>
+      <div className="presentation"></div>
     </header>
-  )
+  );
 }
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
-import styles from "./product.module.css"
+import styles from "./product.module.css";
+import { FiHeart } from "react-icons/fi";
 
 export class Product extends Component {
   render() {
-      return (
-          <div className={styles.product_item}>
-              <img src={"./img/" + this.props.item.img} alt="Product" width="300px" height="300px" className={styles.product_img} />
-              <h2 className={styles.product_title}>{this.props.item.title}</h2>
-              <p className={styles.product_desc}>{this.props.item.desc}</p>
-              <p className={styles.product_category}>{this.props.item.category}</p>
-              <span className={styles.product_price}>{this.props.item.price} $</span>
-              <div className={styles.product_add_card}>+</div>
-          </div>
-      );
+    return (
+      <div className={styles.product_item}>
+        <img
+          src={"./img/" + this.props.item.img}
+          alt="Product"
+          width="300px"
+          height="300px"
+          className={styles.product_img}
+        />
+        <div className="product_card_options">
+          <h2 className={styles.product_title}>{this.props.item.title}</h2>
+          <p className={styles.product_desc}>{this.props.item.desc}</p>
+          <p className={styles.product_category}>{this.props.item.category}</p>
+          <span className={styles.product_price}>
+            {this.props.item.price} $
+          </span>
+        </div>
+        <FiHeart className={styles.product_add_card} />
+      </div>
+    );
   }
 }
 
